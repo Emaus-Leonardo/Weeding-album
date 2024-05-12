de5ed406727d229092438412899263726f8dd3bb
Emaus-Leonardo <126693677+Emaus-Leonardo@users.noreply.github.com>
2024-05-12 12:14:27 -0300

    ajustes

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../assets/components/pages/Home.jsx"
+import PhotoAlbum from "../assets/components/pages/PhotoAlbum.jsx"
+import About from "../assets/components/pages/About.jsx"
 import Footer from "../assets/components/layout/Footer.jsx";
 import Header from "../assets/components/layout/Header.jsx";
 
@@ -9,6 +11,8 @@ const RoutesApp = () => {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/photo-album" element={<PhotoAlbum />} />
+        <Route path="/about" element={<About />} />
       </Routes>
       <Footer />
     </BrowserRouter>
